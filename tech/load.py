222959dabe34900b2b4c119bc5710b68a9e89b48
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-06-19 09:58:42 +0100

    generic techlingos

--- tech/load.py
+++ tech/load.py
@@ -5,9 +5,28 @@ from typing import Generator, Any
 from tech.lingo import Lingo
 
 
+def load_base_lingos() -> Generator[Lingo, Any, None]:
+    lingos_path = Path("lingos")
+    # Read base lingos, EN language
+    for file in lingos_path.glob("*.json"):
+        category = file.stem
+        if "-" in category:
+            continue
+        with open(file) as readable:
+            lingos = json.load(readable)
+            for lingo_json in lingos:
+                lingo = Lingo(
+                    category=category,
+                    language="en",
+                    **lingo_json,
+                )
+                yield lingo
+
 def load_lingos() -> Generator[Lingo, Any, None]:
     lingos_path = Path("lingos")
-    for file in lingos_path.glob("*.json"):
+
+    yield from load_base_lingos()
+    for file in lingos_path.glob("*-*.json"):
         category, _, language = file.stem.partition("-")
 
         with open(file) as readable:
