999e072c107cee2583ba3b591d1c3dfe69cd403e
Antonio Feregrino <antonio.feregrino@gmail.com>
2021-06-20 13:30:57 +0100

    add style checking

--- tech/format.py
+++ tech/format.py
@@ -6,13 +6,16 @@ from typing import Dict, List
 from slugify import slugify
 
 from tech.lingo import Lingo
-from tech.load import load_lingos
+from tech.load import load_base_lingos, load_extra_lingos
 
 
 def format():
     files: Dict[str, List[Lingo]] = defaultdict(list)
-    for lingo in load_lingos():
+    for lingo in load_base_lingos():
+        files[f"{lingo.category}"].append(lingo)
+    for lingo in load_extra_lingos():
         files[f"{lingo.category}-{lingo.language}"].append(lingo)
+
     lingos = Path("lingos")
     for t, lists in files.items():
         jsons = []
@@ -22,6 +25,7 @@ def format():
             jsons.append(__lingo)
         with open(lingos / f"{t}.json", "wt", encoding="utf8") as w:
             json.dump(jsons, w, indent=2, ensure_ascii=False)
+            w.write("\n")
 
 
 if __name__ == "__main__":
