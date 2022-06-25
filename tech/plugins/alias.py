0337a9485dab0da39010bab225d1e2ec32307da6
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-06-25 16:37:30 +0100

    Fix linting

--- tech/plugins/alias.py
+++ tech/plugins/alias.py
@@ -1,5 +1,5 @@
-import os.path
 import logging
+import os.path
 from pathlib import Path
 from urllib.parse import urlparse
 
@@ -16,11 +16,11 @@ class AliasGenerator(object):
     def __init__(self, context, settings, path, theme, output_path, *args):
         self.output_path = output_path
         self.context = context
-        self.alias_delimiter = settings.get('ALIAS_DELIMITER', ',')
+        self.alias_delimiter = settings.get("ALIAS_DELIMITER", ",")
 
     def create_alias(self, page, alias):
         # If path starts with a / add index.html
-        if alias[-1] == '/':
+        if alias[-1] == "/":
             relative_alias = Path(alias, "index.html")
         else:
             relative_alias = Path(alias)
@@ -28,32 +28,34 @@ class AliasGenerator(object):
         path = Path(self.output_path, relative_alias)
 
         try:
-            path.parent.mkdir(parents=True,exist_ok=True)
+            path.parent.mkdir(parents=True, exist_ok=True)
         except OSError:
             pass
 
-        logger.info('[alias] Writing to alias file %s' % path)
-        with open(path, 'w') as fd:
+        logger.info("[alias] Writing to alias file %s" % path)
+        with open(path, "w") as fd:
             destination = page.url
             # if schema is empty then we are working with a local path
             if not urlparse(destination).scheme:
                 # if local path is missing a leading slash then add it
-                if not destination.startswith('/'):
-                    destination = '/{0}'.format(destination)
+                if not destination.startswith("/"):
+                    destination = "/{0}".format(destination)
             fd.write(self.TEMPLATE.format(destination=destination))
 
     def generate_output(self, writer):
         pages = (
-            self.context['pages'] + self.context['articles'] +
-            self.context.get('hidden_pages', []))
+            self.context["pages"]
+            + self.context["articles"]
+            + self.context.get("hidden_pages", [])
+        )
 
         for page in pages:
-            aliases = page.metadata.get('alias', [])
+            aliases = page.metadata.get("alias", [])
             if type(aliases) != list:
                 aliases = aliases.split(self.alias_delimiter)
             for alias in aliases:
                 alias = alias.strip()
-                logger.info('[alias] Processing alias %s' % alias)
+                logger.info("[alias] Processing alias %s" % alias)
                 self.create_alias(page, alias)
 
 
